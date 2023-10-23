293db0ff524915dbc64e2fe0d1f316971c4b747c
remintroy <86014784+remintroy@users.noreply.github.com>
2023-10-23 16:47:04 +0530

    chore: update webserver initialization

--- src/lib/webserver/index.ts
+++ src/lib/webserver/index.ts
@@ -1,12 +1,15 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export default function serverConfig(server: any) {
-  const startServer = () => {
-    server.listen(config.server.port, () => {
-      console.log(`[${config.server.id}] ${config.server.name} is started on ${config.server.port}`);
-    });
-  };
+import express from "express";
+import http from "http";
+import expressConfig from "./express";
 
-  return {
-    startServer,
-  };
+const app = express();
+const server = http.createServer(app);
+
+expressConfig(app, express);
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export default function startWebServer() {
+  server.listen(config.server.port, () => {
+    console.log(`[${config.server.id}] ${config.server.name} is started on ${config.server.port}`);
+  });
 }
