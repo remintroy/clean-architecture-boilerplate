38700152e57092521924c7d02aaed04a0167b875
remintroy <reminremin01@gmail.com>
2023-10-23 16:47:04 +0530

    Webserver Initialization update

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,7 @@
 import "./globals";
-import express from "express";
-import http from "http";
 import connectToDb from "./lib/database/connection";
-import serverConfig from "./lib/webserver";
-import expressConfig from "./lib/webserver/express";
-
-const app = express();
-const server = http.createServer(app);
-
-expressConfig(app, express);
-
-serverConfig(server).startServer();
+import startWebServer from "./lib/webserver";
 
 connectToDb();
+
+startWebServer();
